6d7573ebec1cc5fb7bf04000ade19910a77d97f4
Vladislav2123 <mangushev211104@gmail.com>
2023-11-01 16:35:48 +0300

    Validators for Review commands added

---
+++ src/Core/LibraryApp.Application/Feauters/Reviews/Commands/Delete/DeleteReviewCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace LibraryApp.Application.Feauters.Reviews.Commands.Delete
+{
+	public class DeleteReviewCommandValidator : AbstractValidator<DeleteReviewCommand>
+	{
+        public DeleteReviewCommandValidator()
+        {
+            RuleFor(command => command.UserId)
+                .NotNull()
+                .NotEmpty();
+
+			RuleFor(command => command.ReviewId)
+				.NotNull()
+				.NotEmpty();
+		}
+    }
+}
---
+++ src/Core/LibraryApp.Application/Feauters/Reviews/Commands/Update/UpdateReviewCommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace LibraryApp.Application.Feauters.Reviews.Commands.Update
+{
+	public class UpdateReviewCommandValidator : AbstractValidator<UpdateReviewCommand>
+	{
+        public UpdateReviewCommandValidator()
+        {
+			RuleFor(command => command.UserId)
+				.NotNull()
+				.NotEmpty();
+
+			RuleFor(command => command.ReviewId)
+				.NotNull()
+				.NotEmpty();
+
+			RuleFor(command => command.Rating)
+				.NotEmpty()
+				.GreaterThanOrEqualTo(1)
+				.LessThanOrEqualTo(5);
+
+			RuleFor(command => command.Title)
+				.MaximumLength(50);
+
+			RuleFor(command => command.Text)
+				.MaximumLength(1000);
+		}
+    }
+}
