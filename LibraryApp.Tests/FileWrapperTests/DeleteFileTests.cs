ec1831c11ad64c7467a093e3eb0011d3379939d1
Vladislav2123 <mangushev211104@gmail.com>
2023-12-21 13:39:25 +0300

    FileWrapper tests added

---
+++ LibraryApp.Tests/FileWrapperTests/DeleteFileTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using LibraryApp.Application.Common.FileWrappers;
+
+namespace LibraryApp.Tests.FileWrapperTests;
+public class DeleteFileTests
+{
+	[Fact]
+	public async Task DeleteFile_ExpectedData_Success()
+	{
+		// Arrange
+		var fileWrapper = new FileWrapper();
+		string directory = Path.GetTempPath();
+		string path = Path.Combine(directory, "file.txt");
+
+		File.Create(path).Close();
+
+		// Act
+		fileWrapper.DeleteFile(path);
+
+		// Assert
+		Path.Exists(path).Should().BeFalse();
+	}
+}
---
+++ LibraryApp.Tests/FileWrapperTests/SaveFileTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using LibraryApp.Application.Common.FileWrappers;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace LibraryApp.Tests.FileWrapperTests;
+public class SaveFileTests
+{
+	private readonly FileWrapper _fileWrapper = 
+		new FileWrapper();
+
+	private readonly Mock<IFormFile> _fileMock =
+		new Mock<IFormFile>();
+
+	[Fact]
+	public async Task SaveFileAsync_ExpectedData_Success()
+	{
+		// Arrange
+		string directory = Path.GetTempPath();
+		string path = Path.Combine(directory, "file.txt");
+
+		// Act
+		await _fileWrapper.SaveFileAsync(
+			_fileMock.Object, 
+			path, 
+			CancellationToken.None);
+
+		// Assert
+		Path.Exists(path).Should().BeTrue();
+
+		File.Delete(path);
+	}
+}
